7efeafed6e2a93d9cacedcab1aecc123c8ba1620
callmejojow <73418982+callmejojow@users.noreply.github.com>
2021-08-03 23:42:00 -0400

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+@extends ('layout')
+
+@section ('content')
+<body>
+	<article>
+		<h1>
+			{{ $post->title }}
+		</h1>
+
+		<a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+
+		<div>
+			{!! $post->body !!}
+		</div>
+
+	</article>
+	<a href="/">Go Back</a>
+</body>
+@endsection
